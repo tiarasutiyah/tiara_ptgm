a0cef40b070e71ab8170e426f0f9fa772a2ec681
tiarasutiyah <tiarasutiyah80@gmail.com>
2025-11-04 15:56:56 +0700

    Update latihan2.py

--- latihan2.py
+++ latihan2.py
@@ -1,24 +1,21 @@
-# Deklarasi Class Induk
 class Kendaraan:
     def __init__(self, merk, kecepatan):
         self.merk = merk
         self.kecepatan = kecepatan 
 
-# Subclass Kucing
 class BMW(Kendaraan):
     def info(self):
         print(f"Mobil {self.merk} melaju {self.kecepatan} km/jam.")
 
 
-# Subclass Anjing
-class Porsche(Kendaraan):
+class Honda(Kendaraan):
     def info(self):
-        print(f"Mobil {self.merk} melaju {self.kecepatan} km/jam.")
+        print(f"Motor {self.merk} melaju {self.kecepatan} km/jam.")
 
 
 # Membuat objek dari subclass
 b1 = BMW("BMW", 800)
-p1 = Porsche("Porsche", 400)
+p1 = Honda("Honda", 400)
 
 # Memanggil method
 b1.info()
